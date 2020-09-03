dd2ed0190a150feb9a343b7cdf2b3539b6ade193
abhinav-TB <abhinavtb@gmail.com>
2020-09-03 15:24:16 +0530

    added names

--- conference_frontend/src/components/commitie list/list.js
+++ conference_frontend/src/components/commitie list/list.js
@@ -8,26 +8,31 @@ export default function list() {
                 <h2><center>ADVISORY COMMITTEE</center></h2>
             <div className="advisory">
                 <div>
-                 <ul style={{listStyleType:"none"}}>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor,<br/> CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
-                     <li> Patron: Dr. K. N. Madhusoodanan, Vice Chancellor, CUSAT.</li>
+                 <ul style={{listStyleType:"none",width:"100%"}}>
+                     <li > Patron: Dr. K. N. Madhusoodanan, Vice Chancellor,<br/> CUSAT.</li>
+                     <li> Amit Bhattacharia, President, International ,<br/> Indian Statistical Association, India.</li>
+                     <li> Dan Jeske, University of California, Riverside,<br/>USA.</li>
+                     <li>Galit Shmueli, National Tsing Hua University, <br/>Taiwan</li>
+                     <li> Hira Lal Koul, Michigan State University.</li>
+                     <li>A.J. Lawrance, University of Warwick, UK.</li>
+                     <li>K. R. Muraleedharan Nair, CUSAT, India.</li>
+                     <li>K. R. Murali Mohan, SERB, Department of Science & <br/>Technology, Govt of India.</li>
+                     <li> William Meeker, Iowa State University, USA.</li>
+                    
                   
                  </ul>
                 </div>
                 <div>
                     <ul style={{listStyleType:"none"}}>
                     <li>K. Srinivasa Rao, President, Indian Society of Probability<br/> & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
+                    <li>K. Suresh Chandra, Madras University, India</li>
+                    <li>N. Balakrishnan, McMaster University, Canada.</li>
+                    <li>N. Unnikrishnan Nair, CUSAT, India.</li>
+                    <li>Rajeev L. Karandikar, Chennai Mathematical Institute,<br/> India.</li>
+                    
+                    <li>Rao S. Jammalamadaka, University of California,<br/> Santa Barbara, USA.</li>
+                    <li>S. P. Mukherjee, Calcutta University, India.</li>
+                    <li>Sunitha Bhaskar, Dy Director General, NSSO, Govt of India <br/>V. K. Ramachandran Nair, CUSAT, India.</li>
                     
                    
                     </ul>
@@ -41,30 +46,36 @@ export default function list() {
             <div className="scientific">
             <h2><center>SCIENTIFIC PROGRAM COMMITTEE</center></h2>
             <ul style={{listStyleType:"none"}}>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
+                    <li>Chair: Asha Gopalakrishnan, CUSAT, India.</li>
+                    <li>Anandamayee Majumdhar, North South University, Dhaka,<br/> Bangladesh.</li>
+                    <li>Anup Dewanji, ISI Kolkota, India.</li>
+                    <li>Balaji Raman, Cogitaas Technologies Inc, India.</li>
+                    <li>Bovas Abraham, University of Waterloo, Canada.</li>
+                    <li>Debasis Kundu, IIT Kanpur, India.</li>
+                    <li>Fabrizio Ruggeri, CNR IMATI Milano, Italy.</li>
+                    <li>Isha Dewan, ISI Delhi, India.</li>
+                    <li>P. G. Sankaran, CUSAT. India.</li>
+                    <li>Pushpa Wijecon, University of Peradenia., Sri Lanka.</li>
+                    <li>R. P. Suresh, Accenture Analytics, India.</li>
+                    <li>Rituparna Sen, ISI Chennai, India.</li>
+                    <li>T. Rajesh, Caterpillar, India.</li>
+                    <li>T. V. Ramanathan, Pune University, India.</li>
+                    <li>Wee Yeap Lau,University of Malaya, Malaysia.</li>
                    
                     </ul>
             </div>
             <div className="scientific">
             <h2><center>LOCAL ORGANIZING COMMITTEE</center></h2>
             <ul style={{listStyleType:"none"}}>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
-                    <li>K. Srinivasa Rao, President, Indian Society of Probability & Statistics, India.</li>
+                    <li>Chair: Balakrishna N, CUSAT.</li>
+                    <li>C Satheesh Kumar, President, Kerala Statistical<br/> Association., India.</li>
+                    <li>Irshad, M. R. CUSAT,India.</li>
+                    <li>K. C. James, CUSAT, India.</li>
+                    <li>K. G. Geetha, CUSAT, India.</li>
+                    <li>N. Hitha, CUSAT, India.</li>
+                    <li>Rajesh G., CUSAT, India.</li>
+                    <li>S. M. Sunoj,K CUSAT, India.</li>
+                    <li>Sudheeshkumar Kattumannil, ISI Chennai, India.</li>
                    
                     </ul>
             </div>
